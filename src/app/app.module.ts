488dc35f3b6923405b10e4e483ea7a88cf67bef7
ANANTH4004 <balamuruganananth@gmail.com>
2023-06-23 00:06:40 +0530

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataStoreComponent } from './data-store/data-store.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './Material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataStoreComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-store/data-store.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+const data : any[] = [
+  {
+    title : "Passenger Booking",
+
+    records : "2345",
+
+    fields : "10",
+
+    lastModified : "10 hours ago",
+
+    modifiedBy : "Auto update",
+
+    description : "This dataset contains the passenger booking data",
+
+    path : "assets/statics.png"
+
+  },
+
+  {
+
+    title : "Ticketing",
+
+    records : "2345",
+
+    fields : "10",
+
+    lastModified : "8 hours ago",
+
+    modifiedBy : "Auto update",
+
+    description : "This dataset contains the passenger booking data",
+
+    path : "assets/statics.png"
+
+  },
+
+  {
+
+    title : "Customer Order",
+
+    records : "2345",
+
+    fields : "10",
+
+    lastModified : "7 hours ago",
+
+    modifiedBy : "Auto update",
+
+    description : "This dataset contains the passenger booking data",
+
+    path : "assets/statics.png"
+
+  },
+
+  {  
+
+    title : "Baggage",
+
+    records : "2345",
+
+    fields : "10",
+
+    lastModified : "1 hours ago",
+
+    modifiedBy : "Auto update",
+
+    description : "This dataset contains the passenger booking data",
+
+    path : "assets/dbpic.jpg"
+
+  }, {
+
+    title : "Baggage",
+
+    records : "2345",
+
+    fields : "10",
+
+    lastModified : "10 hours ago",
+
+    modifiedBy : "Auto update",
+
+    description : "This dataset contains the passenger booking data",
+
+    path : "assets/dbpic.jpg"
+
+  }
+
+]
+@Component({
+  selector: 'app-data-store',
+  templateUrl: './data-store.component.html',
+  styleUrls: ['./data-store.component.scss']
+})
+export class DataStoreComponent {
+
+  data1 : any[]= data;
+
+}
